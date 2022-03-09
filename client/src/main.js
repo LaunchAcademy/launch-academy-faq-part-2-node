754c84fadb906cac2b0c80a649f2172fab03eec4
Stephen Fang <safang99@gmail.com>
2022-03-09 14:26:10 -0500

    starter code

---
+++ client/src/main.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FAQList from './components/FAQList'
+
+ReactDOM.render(
+  <FAQList />,
+  document.getElementById('app')
+);
